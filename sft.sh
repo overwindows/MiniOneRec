58675a9dc824850d7a1e08eddcfec4b534170bb0
Chen Wu <chenw@sambanovasystems.com>
2025-12-07 21:42:45 +0000

    Enhance sft.sh script with environment variables and dynamic parameters for training. Set NCCL_IB_DISABLE, WANDB_API_KEY, and updated model path. Adjusted torchrun parameters for process number and output directory naming.

--- sft.sh
+++ sft.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
 export NCCL_IB_DISABLE=1        # 完全禁用 IB/RoCE
+export WANDB_API_KEY=fd3aec2cadf8ee9a2b3c6f4ac8210f65d73d134b
+
+PROCESS_NUM=4
+MODEL_PATH=/nvmedata/hf_checkpoints/Qwen2.5-1.5B-Instruct
 # Office_Products, Industrial_and_Scientific
 for category in "Industrial_and_Scientific"; do
     train_file=$(ls -f ./data/Amazon/train/${category}*11.csv)
@@ -7,20 +13,20 @@ for category in "Industrial_and_Scientific"; do
     info_file=$(ls -f ./data/Amazon/info/${category}*.txt)
     echo ${train_file} ${eval_file} ${info_file} ${test_file}
     
-    torchrun --nproc_per_node 8 \
+    torchrun --nproc_per_node ${PROCESS_NUM} \
             sft.py \
-            --base_model your_model_path \
+            --base_model ${MODEL_PATH} \
             --batch_size 1024 \
             --micro_batch_size 16 \
             --train_file ${train_file} \
             --eval_file ${eval_file} \
-            --output_dir output_dir/xxx \
-            --wandb_project wandb_proj \
-            --wandb_run_name wandb_name \
+            --output_dir output_dir/sft_${category}_qwen1.5b_bs1024 \
+            --wandb_project MiniOneRec \
+            --wandb_run_name sft_${category}_qwen1.5b_bs1024 \
             --category ${category} \
             --train_from_scratch False \
             --seed 42 \
             --sid_index_path ./data/Amazon/index/Industrial_and_Scientific.index.json \
             --item_meta_path ./data/Amazon/index//Industrial_and_Scientific.item.json \
-            --freeze_LLM False
+            --freeze_LLM False \
 done
